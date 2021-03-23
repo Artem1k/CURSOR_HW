afa30d30faa89f7a2bbb8607c4ec8b5d43fa5c8e
Artem Boiko <artemboyko2002@gmail.com>
2021-03-23 19:51:56 +0200

    HW9 Заміна logger на logging. added comments. added methods to class CleanerWork

--- HW9_Logging_and_Exceptions.py
+++ HW9_Logging_and_Exceptions.py
@@ -2,28 +2,27 @@ import logging
 import inspect
 import time
 
-# import time
-# time.asctime()
-# log_template = '%(asctime)s - %(name)s - %(levelname)s - %(message)s - %(pathname)s'
-# logging.basicConfig(level=logging.DEBUG, filename="test.log", filemode="w", format=log_template)
-logger = logging.getLogger(__name__)
-
-# Create handlers
-c_handler = logging.StreamHandler()
-f_handler = logging.FileHandler('file.log', 'w')
-
-c_handler.setLevel(logging.ERROR)
-f_handler.setLevel(logging.DEBUG)
-
-# Create formatters and add it to handlers
-c_format = logging.Formatter('%(name)s - %(levelname)s - %(message)s')
-f_format = logging.Formatter('%(asctime)s - %(name)s - %(levelname)s - %(message)s')
-c_handler.setFormatter(c_format)
-f_handler.setFormatter(f_format)
-
-# Add handlers to the logger
-logger.addHandler(c_handler)
-logger.addHandler(f_handler)
+time.asctime()
+log_template = '%(asctime)s - %(name)s - %(levelname)s - %(message)s - %(pathname)s'
+logging.basicConfig(level=logging.DEBUG, filename="test.log", filemode="w", format=log_template)
+# logger = logging.getLogger(__name__)
+#
+# # Create handlers
+# c_handler = logging.StreamHandler()
+# f_handler = logging.FileHandler('file.log', 'w')
+#
+# c_handler.setLevel(logging.ERROR)
+# f_handler.setLevel(logging.DEBUG)
+#
+# # Create formatters and add it to handlers
+# c_format = logging.Formatter('%(name)s - %(levelname)s - %(message)s')
+# f_format = logging.Formatter('%(asctime)s - %(name)s - %(levelname)s - %(message)s')
+# c_handler.setFormatter(c_format)
+# f_handler.setFormatter(f_format)
+#
+# # Add handlers to the logger
+# logger.addHandler(c_handler)
+# logger.addHandler(f_handler)
 print('Це калькулятор!')
 
 
@@ -31,27 +30,27 @@ print('Це калькулятор!')
 def number():
     while True:
         str_num = input('')
-        logger.info(f'str_num in def number(): {str_num}. '
-                    f'- {inspect.stack()[1].code_context[0][-1]} in line: {inspect.stack()[1].lineno}')
+        logging.info(f'str_num in def number(): {str_num}. '
+                     f'- {inspect.stack()[1].code_context[0][-1]} in line: {inspect.stack()[1].lineno}')
         try:
             num = int(str_num)
             break
         except ValueError:
-            logger.error('ValueError in def number()')
+            logging.error('ValueError in def number()')
             print('ValueError')
-    logger.info(f'num in def number(): {num}. '
-                f'- {inspect.stack()[1].code_context[0][-1]} in line: {inspect.stack()[1].lineno}')
+    logging.info(f'num in def number(): {num}. '
+                 f'- {inspect.stack()[1].code_context[0][-1]} in line: {inspect.stack()[1].lineno}')
     return num
 
 
 while True:
     str_point = input('Що ви хочете виконати?\n1.Додавання\n2.Віднімання\n3.Множення\n4.Ділення\n'
                       '5.Піднесення в степінь\n6.Взяття з під кореня\n7.Пошук відсотку від числа\n8.Exit\n')
-    logger.info(f'str_point: {str_point}')
+    logging.info(f'str_point: {str_point}')
     try:
         point = int(str_point)
     except ValueError:
-        logger.error('ValueError when tried to int(str_point)')
+        logging.error('ValueError when tried to int(str_point)')
         print('Incorrect!')
     if point == 1:
         print('Введіть перший доданок: ', end='')
@@ -59,7 +58,7 @@ while True:
         print('Введіть другий доданок: ', end='')
         num2 = number()
         result = num1 + num2
-        logger.info(f'Сума: {result}')
+        logging.info(f'Сума: {result}')
         print(f'Сума: {result}')
     elif point == 2:
         print('Введіть зменшуване: ', end='')
@@ -67,7 +66,7 @@ while True:
         print('Введіть від\'ємник: ', end='')
         num2 = number()
         result = num1 - num2
-        logger.info(f'Різниця: {result}')
+        logging.info(f'Різниця: {result}')
         print(f'Різниця: {result}')
     elif point == 3:
         print('Введіть перший множник: ', end='')
@@ -75,7 +74,7 @@ while True:
         print('Введіть другий множник: ', end='')
         num2 = number()
         result = num1 * num2
-        logger.info(f'Добуток: {result}')
+        logging.info(f'Добуток: {result}')
         print(f'Добуток: {result}')
     elif point == 4:
         print('Введіть ділене: ', end='')
@@ -85,9 +84,9 @@ while True:
         try:
             result = num1 / num2
         except ZeroDivisionError:
-            logger.warning('ZeroDivisionError')
+            logging.warning('ZeroDivisionError')
             result = 'infinity'
-        logger.info(f'Частка: {result}')
+        logging.info(f'Частка: {result}')
         print(f'Частка: {result}')
     elif point == 5:
         print('Введіть число: ', end='')
@@ -95,7 +94,7 @@ while True:
         print('Введіть степінь: ', end='')
         num2 = number()
         result = num1 ** num2
-        logger.info(f'Результат: {result}')
+        logging.info(f'Результат: {result}')
         print(f'Результат: {result}')
     elif point == 6:
         print('Введіть число: ', end='')
@@ -103,14 +102,14 @@ while True:
         print('Введіть степінь кореня: ', end='')
         num2 = number()
         result = num1 ** (1 / num2)
-        logger.info(f'Результат: {result}')
+        logging.info(f'Результат: {result}')
         print(f'Результат: {result}')
     elif point == 7:
         def sub_number():
             while True:
                 num = number()
                 if num < 0:
-                    print('Процент не може бути мінусовий')
+                    print('Відсоток не може бути мінусовий')
                 else:
                     return num
                     break
@@ -121,15 +120,86 @@ while True:
         print('Введіть відсотки: ', end='')
         num2 = sub_number()
         result = num1 * (num2 / 100)
-        logger.info(f'Результат: {result}')
+        logging.info(f'Результат: {result}')
         print(f'Результат: {result}')
     elif point == 8:
-        logger.info('exit')
+        logging.info('exit')
         print('exit')
         break
     else:
-        logger.warning(f'Incorrect number of act!')
+        logging.warning(f'Incorrect number of act!')
         print('Incorrect!')
+
+
+'''Це калькулятор!
+Що ви хочете виконати?
+1.Додавання
+2.Віднімання
+3.Множення
+4.Ділення
+5.Піднесення в степінь
+6.Взяття з під кореня
+7.Пошук відсотку від числа
+8.Exit
+1
+Введіть перший доданок: 4
+Введіть другий доданок: h
+ValueError
+6
+Сума: 10
+Що ви хочете виконати?
+1.Додавання
+2.Віднімання
+3.Множення
+4.Ділення
+5.Піднесення в степінь
+6.Взяття з під кореня
+7.Пошук відсотку від числа
+8.Exit
+7
+Введіть число: 100
+Введіть відсотки: -5
+Процент не може бути мінусовий
+5
+Результат: 5.0
+Що ви хочете виконати?
+1.Додавання
+2.Віднімання
+3.Множення
+4.Ділення
+5.Піднесення в степінь
+6.Взяття з під кореня
+7.Пошук відсотку від числа
+8.Exit
+4
+Введіть ділене: 5
+Введіть дільник: 0
+Частка: infinity
+Що ви хочете виконати?
+1.Додавання
+2.Віднімання
+3.Множення
+4.Ділення
+5.Піднесення в степінь
+6.Взяття з під кореня
+7.Пошук відсотку від числа
+8.Exit
+6
+Введіть число: 25
+Введіть степінь кореня: 2
+Результат: 5.0
+Що ви хочете виконати?
+1.Додавання
+2.Віднімання
+3.Множення
+4.Ділення
+5.Піднесення в степінь
+6.Взяття з під кореня
+7.Пошук відсотку від числа
+8.Exit
+8
+exit
+'''
 
 
 # Task 2
@@ -157,6 +227,7 @@ class CleanerWork:
 
     def move(self):
         while True:
+            time.sleep(1)
             print('move')
             try:
                 self.vacuum_cleaner()
@@ -219,5 +290,16 @@ class CleanerWork:
         else:
             raise FullTrash
 
-    def recharger(self):
+    def recharge(self):
         self.battery = 100
+
+    def to_clean(self):
+        self.trash = 0
+
+    def add_water(self):
+        self.water = 500
+
+
+vacuum_water = 500
+vacuum_trash = 0
+vacuum_battery = 100
