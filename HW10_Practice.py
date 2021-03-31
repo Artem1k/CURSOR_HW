da1e15b3d0a0afb6489d29c5fa401eb6f460ba6b
Artem Boiko <artemboyko2002@gmail.com>
2021-04-01 02:07:24 +0300

    HW10 unittest

---
+++ HW10_Practice.py
@@ -0,0 +1,37 @@
+import unittest
+from VacuumCleaner import *
+
+
+class TestCleanerWork(unittest.TestCase):
+    def setUp(self) -> None:
+        self.cleaner_1 = CleanerWork(100, 200, 500)
+        self.cleaner_2 = CleanerWork(50, 35, 200)
+        self.cleaner_3 = CleanerWork('75', 110.5, 250.9)
+        self.cleaner_4 = CleanerWork('0', 0.0, 0)
+        self.cleaners = [self.cleaner_1, self.cleaner_2, self.cleaner_3, self.cleaner_4]
+
+    def test_init(self):
+        with self.assertRaises(ValueError):
+            CleanerWork('one', 0, 0)
+            CleanerWork(-1, -1, 1000)
+        for cleaner in self.cleaners:
+            self.assertIsInstance(cleaner.battery, int)
+            self.assertIsInstance(cleaner.trash, int)
+            self.assertIsInstance(cleaner.water, int)
+
+    def test_move(self):
+        for cleaner in self.cleaners:
+            with self.subTest(battery=cleaner.battery, trash=cleaner.trash, water=cleaner.water):
+                cleaner.move()
+
+    def test_recharge(self):
+        for cleaner in self.cleaners:
+            cleaner.recharge()
+
+    def test_to_clean(self):
+        for cleaner in self.cleaners:
+            cleaner.to_clean()
+
+    def test_add_water(self):
+        for cleaner in self.cleaners:
+            cleaner.add_water()
---
+++ VacuumCleaner.py
@@ -0,0 +1,105 @@
+class LowBattery(Exception):
+    pass
+
+
+class DeadBattery(Exception):
+    pass
+
+
+class EmptyWater(Exception):
+    pass
+
+
+class FullTrash(Exception):
+    pass
+
+
+class CleanerWork:
+    def __new__(cls, battery, trash, water):
+        if (int(battery) in range(101)) and (int(trash) in range(201)) and (int(water) in range(501)):
+            return object.__new__(cls)
+        assert ('d2f3vervrfdsg')
+
+    def __init__(self, battery, trash, water):
+        self.battery = int(battery)
+        self.trash = int(trash)
+        self.water = int(water)
+
+    def move(self):
+        while True:
+            # time.sleep(1)
+            # print('move')
+            try:
+                self.low_battery_check()
+                self.vacuum_cleaner()
+                self.wash()
+            except EmptyWater:
+                while True:
+                    try:
+                        self.low_battery_check()
+                        self.vacuum_cleaner()
+                    except LowBattery:
+                        for i in range(20):
+                            try:
+                                self.vacuum_cleaner()
+                            except FullTrash:
+                                print('Почистіть мене від сміття!')
+                                break
+                    except DeadBattery:
+                        print('Занесіть мене на зарядку!')
+                        break
+                    except FullTrash:
+                        print('Почистіть мене від сміття!')
+                        break
+            except LowBattery:
+                for i in range(20):
+                    try:
+                        self.vacuum_cleaner()
+                        self.wash()
+                    except EmptyWater:
+                        while i < 19:
+                            try:
+                                i += 1
+                                self.vacuum_cleaner()
+                            except FullTrash:
+                                print('Почистіть мене від сміття!')
+                                break
+                    except FullTrash:
+                        print('Почистіть мене від сміття!')
+                        break
+            except DeadBattery:
+                print('Занесіть мене на зарядку!')
+                break
+            except FullTrash:
+                print('Почистіть мене від сміття!')
+                break
+        print('Я закінчив!')
+
+    def wash(self):
+        if self.water > 1:
+            self.water -= 2
+        else:
+            raise EmptyWater
+
+    def low_battery_check(self):
+        if self.battery == 20:
+            raise LowBattery
+
+    def vacuum_cleaner(self):
+        if self.battery > 0 and self.trash != 200:
+            self.battery -= 1
+        elif self.battery == 0:
+            raise DeadBattery
+        if self.trash < 200:
+            self.trash += 1
+        else:
+            raise FullTrash
+
+    def recharge(self):
+        self.battery = 100
+
+    def to_clean(self):
+        self.trash = 0
+
+    def add_water(self):
+        self.water = 500
