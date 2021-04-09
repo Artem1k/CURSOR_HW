e7f140e782607c2d2740495b9fc884f0d33ad4c2
Artem Boiko <artemboyko2002@gmail.com>
2021-04-09 16:48:19 +0300

    HW12 ex2

---
+++ HW12_argument_parser_1.py
@@ -0,0 +1,68 @@
+import argparse
+import csv
+
+'''exp; required: false; default: min(exp)
+current_job_exp; required: false; default: max(current_job_exp)
+sex; required: false
+city; required: false
+position; required: false
+age; required: false
+path_to_source_files; required: true;
+destination_path; required: false; default: .
+destination_filename; required: false; default: f"2020_june_mini.csv".'''
+parser = argparse.ArgumentParser(description='A tutorial of argparse!')
+parser.add_argument('--exp', '-e', required=False, default='min(exp)')
+parser.add_argument('--current_job_exp', '-j', required=False, default='max(current_job_exp)')
+parser.add_argument('--sex', '-s', required=False)
+parser.add_argument('--city', '-c', required=False)
+parser.add_argument('--position', '-p', required=False)
+parser.add_argument('--age', '-a', required=False)
+parser.add_argument('--path_to_source_files', '-t', required=True)
+parser.add_argument('--destination_path', '-P', required=False, default='.')
+parser.add_argument('--destination_filename', '-F', required=False, default=f"2020_june_mini.csv")
+args = parser.parse_args()
+'''The script should read the .csv file and get the information based on your input and generate a new .csv
+file with that info
+
+Example of input:
+-exp 3 -sex female -position DevOps -city Kyiv --path_to_source_files . ...'''
+with open(args.path_to_source_files, 'r') as f:
+    reader = csv.DictReader(f)
+    if args.exp == 'min(exp)':
+        lst_exp = []
+        for row in reader:
+            lst_exp.append(float(row['exp']))
+        args.exp = min(lst_exp)
+        del lst_exp
+        f.seek(0)
+    if args.current_job_exp == 'max(current_job_exp)':
+        lst_current_job_exp = []
+        for row in reader:
+            lst_current_job_exp.append(float(row['current_job_exp']))
+        args.current_job_exp = max(lst_current_job_exp)
+        del lst_current_job_exp
+        f.seek(0)
+    with open('new_file.csv', 'w') as new_f:
+        field_names = list(map(lambda x: x.replace('\n', '')[1:-1], next(f).split(',')))
+        writer = csv.DictWriter(new_f, fieldnames=field_names)
+        writer.writeheader()
+        for row in reader:
+            if args.position:
+                if row['Должность'] != args.position:
+                    continue
+            if args.exp:
+                if float(row['exp']) != float(args.exp):
+                    continue
+            if args.current_job_exp:
+                if float(row['current_job_exp']) != float(args.current_job_exp):
+                    continue
+            if args.age:
+                if float(row['age']) != float(args.age):
+                    continue
+            if args.sex:
+                if row['sex'] != args.sex:
+                    continue
+            if args.city:
+                if row['Город'] != args.city:
+                    continue
+            writer.writerow(row)
---
+++ HW12_argument_parser_2.py
@@ -0,0 +1,44 @@
+'''
+source_file_path; required: true;
+start_salary; required: false; help: starting point of salary;
+end_salary; required: false; help: the max point of salary;
+position; required: false; help: position role
+age; required: false; help: Age of person
+language; required: false; help; Programming language
+
+Based on this info generate a new report of average salary.'''
+
+import argparse
+import csv
+
+parser = argparse.ArgumentParser(description='A tutorial of argparse!')
+parser.add_argument('--source_file_path', '-f', required=True)
+parser.add_argument('--start_salary', '-s', required=False, help='starting point of salary')
+parser.add_argument('--end_salary', '-e', required=False, help='the max point of salary;')
+parser.add_argument('--position', '-p', required=False, help='position role')
+parser.add_argument('--age', '-a', required=False, help='Age of person')
+parser.add_argument('--language', '-l', required=False, help='Programming language')
+args = parser.parse_args()
+
+with open(args.source_file_path, 'r') as f:
+    reader = csv.DictReader(f)
+    price_list = []
+    for row in reader:
+        if args.position:
+            if row['Должность'] != args.position:
+                continue
+        if args.age:
+            if int(row['Возраст']) != args.age:
+                continue
+        if args.start_salary:
+            if float(row['Зарплата.в.месяц']) < float(args.start_salary):
+                continue
+        if args.end_salary:
+            if float(row['Зарплата.в.месяц']) > float(args.end_salary):
+                continue
+        price_list.append(float(row['Зарплата.в.месяц']))
+print(round(sum(price_list) / len(price_list), 2))
+'''
+python3 HW12_argument_parser_2.py -f 2020_june_mini.csv -s 12000 
+14472.22
+'''
